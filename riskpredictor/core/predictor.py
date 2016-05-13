06c453d18cae3d5879b6c1ec762ef718111ae1e9
Bjarni Vilhjalmsson <bjarni.vilhjalmsson@gmail.com>
2016-05-13 09:24:21 -0400

    debugging

--- riskpredictor/core/predictor.py
+++ riskpredictor/core/predictor.py
@@ -114,9 +114,9 @@ def validate_predictions(K=1, trait='height'):
     
     print pred_phens
     print pred_res['true_phens']
-    pval_derived_effects_prs = sp.array(pred_res['pval_derived_effects_prs'])
-    pval_derived_effects_prs.shape = (len(pred_phens), 1)
-    Xs = sp.hstack([sp.ones((len(pred_phens), 1)),pval_derived_effects_prs])
+    true_phens = sp.array(pred_res['true_phens'])
+    true_phens.shape = (len(pred_phens), 1)
+    Xs = sp.hstack([sp.ones((len(pred_phens), 1)),true_phens])
     (betas, rss_pd, r, s) = linalg.lstsq(Xs, pred_phens)
     print betas
         
